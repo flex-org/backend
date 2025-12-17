c6729e2e705d02613628dbfd2178d1cddea41e23
Ahmed Tarboush <ahmedaymantarboush@gmail.com>
2025-12-18 00:13:51 +0200

    cylling_system

--- routes/V1/enums.php
+++ routes/V1/enums.php
@@ -7,9 +7,57 @@ use App\Modules\V1\Utilities\enums\BillingCycle;
 use App\Modules\V1\Platforms\Enums\PlatformSellingSystem;
 
     Route::get('selling-systems', function() {
-        return ApiResponse::success(PlatformSellingSystem::options());
+        return ApiResponse::success(SellingSystem::all());
     });
 
     Route::get('billing-cycles', function() {
-        return ApiResponse::success(SellingSystem::all());
+        return ApiResponse::success(BillingCycle::options());
+    });
+
+
+    Route::get('selling-systems/add', function() {
+        $systems = [
+            [
+                'translations' => [
+                    'en' => [
+                        'name' => 'Categories',
+                        'description' => 'Sell by categories (includes all courses within the category)',
+                    ],
+                    'ar' => [
+                        'name' => 'الأقسام',
+                        'description' => 'البيع بالقسم (يشمل جمسع الكورسات داخل القسم)',
+                    ],
+                ],
+            ],
+            [
+                'translations' => [
+                    'en' => [
+                        'name' => 'Courses',
+                        'description' => 'Sell by courses (includes all sessions within the course)',
+                    ],
+                    'ar' => [
+                        'name' => 'الكورسات',
+                        'description' => 'البيع بالكورس (يشمل جميع الحلسات داخل الكورس)',
+                    ],
+                ],
+            ],
+            [
+                'translations' => [
+                    'en' => [
+                        'name' => 'Sessions',
+                        'description' => 'Sell by sessions',
+                    ],
+                    'ar' => [
+                        'name' => 'الجلسات',
+                        'description' => 'البيع بالجلسة المنفردة',
+                    ],
+                ],
+            ],
+        ];
+        foreach ($systems as $systemData) {
+            $system = new SellingSystem();
+            $system->fill($systemData);
+            $system->save();
+        }
+        return ApiResponse::success('Selling systems added successfully');
     });
